a975613ad2942824410adb435d335fab2872e8c6
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-12-13 13:55:54 +0100

    fix problems with current simpleormap changes

--- app/models/Stoodle.php
+++ app/models/Stoodle.php
@@ -157,16 +157,6 @@ class Stoodle extends SimpleORMap
         }
     }
 
-    /**
-     * returns new Stoodle instance for given id when found in db, else null
-     * @param  string $id a stoodle id
-     * @return mixed  a stoodle object or null
-     **/
-    public static function find($id)
-    {
-        return SimpleORMap::find(__CLASS__, $id);
-    }
-
     public static function findByRange($range_id)
     {
         $query = "SELECT stoodle_id FROM stoodle WHERE range_id = ? AND evaluated IS NULL ORDER BY start_date, end_date";
@@ -210,19 +200,6 @@ class Stoodle extends SimpleORMap
             $result[$index][] = $stoodle;
         }
         return $result;
-    }
-
-    /**
-     * returns stoodle object for given id or null
-     * the param could be a string, an assoc array containing primary key field
-     * or an already matching object. In all these cases an object is returned
-     *
-     * @param mixed $id_or_object id as string, object or assoc array
-     * @return Stoodle
-     */
-    public static function toObject($id_or_object)
-    {
-        return SimpleORMap::toObject(__CLASS__, $id_or_object);
     }
 
     public static function getMaxPosition($range_id)
