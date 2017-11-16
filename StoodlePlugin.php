198771b26f9dda7b0e275b3efe710ef61e45244c
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-11-16 15:08:17 +0100

    fix encodings, re #22

--- StoodlePlugin.php
+++ StoodlePlugin.php
@@ -16,7 +16,7 @@ class StoodlePlugin extends StudIPPlugin implements StandardPlugin
         parent::__construct();
 
         bindtextdomain(static::GETTEXT_DOMAIN, $this->getPluginPath() . '/locale');
-        bind_textdomain_codeset(static::GETTEXT_DOMAIN, 'ISO-8859-1');
+        bind_textdomain_codeset(static::GETTEXT_DOMAIN, 'UTF-8');
     }
 
     /**
